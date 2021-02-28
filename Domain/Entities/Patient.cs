98ea6b7de4dd197b3ed6dd93e407a27858987649
Paul DeVito <pdevito3@gmail.com>
2021-02-28 15:35:58 -0500

    basic start to business logic

--- Domain/Entities/Patient.cs
+++ Domain/Entities/Patient.cs
@@ -3,11 +3,14 @@ namespace Domain.Entities
     using System;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using Domain.Enums;
     using Sieve.Attributes;
 
     [Table("Patient")]
     public class Patient
     {
+        private Sex _sex = Enums.Sex.Unknown;
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Required]
@@ -30,16 +33,28 @@ namespace Domain.Entities
         public DateTimeOffset? Dob { get; set; }
 
         [Sieve(CanFilter = true, CanSort = true)]
-        public string Sex { get; set; }
+        public string Sex 
+        {
+            get => Enum.GetName(typeof(Sex), _sex);
+            set
+            {
+                if(value.Trim().Equals("Male", StringComparison.InvariantCultureIgnoreCase) || value.Trim().Equals("M", StringComparison.InvariantCultureIgnoreCase))
+                    _sex = Enums.Sex.Male;
+                else if (value.Trim().Equals("Female", StringComparison.InvariantCultureIgnoreCase) || value.Trim().Equals("F", StringComparison.InvariantCultureIgnoreCase))
+                    _sex = Enums.Sex.Female;
+                else
+                    _sex = Enums.Sex.Unknown;
+            }
+        }
 
         [Sieve(CanFilter = true, CanSort = true)]
         public string Gender { get; set; }
 
         [Sieve(CanFilter = true, CanSort = true)]
-        public string Race { get; set; }
+        public string Race { get; set; } // make a separate class
 
         [Sieve(CanFilter = true, CanSort = true)]
-        public string Ethnicity { get; set; }
+        public string Ethnicity { get; set; } // make a separate class
 
         // add-on property marker - Do Not Delete This Comment
     }
---
+++ Domain/Enums/Sex.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Enums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public enum Sex
+    {
+        Male = 1,
+        Female = 2,
+        Unknown = 3,
+    }
+}
